c4633af6a476455abdd00b42c0e0c035065ce36f
Celeste4321 <112397888+Celeste4321@users.noreply.github.com>
2023-07-01 00:05:50 -0300

    Add files via upload

---
+++ clonazepam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clonazepam',
+  templateUrl: './clonazepam.component.html',
+  styleUrls: ['./clonazepam.component.css']
+})
+export class ClonazepamComponent {
+
+}
