b942b36b0467cf0a6a9d91837a21cef92d7bd59c
martinfazio <112627379+martinfazio@users.noreply.github.com>
2023-07-01 00:06:44 -0300

    Add files via upload

---
+++ paco.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paco',
+  templateUrl: './paco.component.html',
+  styleUrls: ['./paco.component.css']
+})
+export class PacoComponent {
+
+}
