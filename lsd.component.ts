05207442c5587469d0d632efe6b9d352402c5380
martinfazio <112627379+martinfazio@users.noreply.github.com>
2023-07-01 00:13:11 -0300

    Add files via upload

---
+++ lsd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lsd',
+  templateUrl: './lsd.component.html',
+  styleUrls: ['./lsd.component.css']
+})
+export class LSDComponent {
+
+}
