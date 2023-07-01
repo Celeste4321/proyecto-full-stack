de022251d76d025569c64d432ad28b7d8ad6e31c
martinfazio <112627379+martinfazio@users.noreply.github.com>
2023-07-01 00:13:49 -0300

    Add files via upload

---
+++ marihuana.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marihuana',
+  templateUrl: './marihuana.component.html',
+  styleUrls: ['./marihuana.component.css']
+})
+export class MarihuanaComponent {
+
+}
